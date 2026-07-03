ad4f13ba9d1eec6a2979903e594ec16e5a32c40d
Grupo Coinp Deploy <deploy@grupocoinp.com>
2026-07-03 13:00:05 -0500

    fix: agregar argumentos de optimización de memoria a puppeteer para evitar cuelgues en Render

--- server/index.js
+++ server/index.js
@@ -19,7 +19,16 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: [
+      '--no-sandbox', 
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
   }
 });
 
